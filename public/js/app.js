23e4b4035925d431d63037e8205438f92287b0f4
Nic <nicolaas@elements.cloud>
2022-11-22 11:16:10 +0200

    Renamed file

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
+    const location = search.value;
+
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+                console.log(data.error);
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+                console.log(data.location, data.forecast);
+            }
+        })
+    })
+})
